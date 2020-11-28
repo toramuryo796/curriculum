ca1211a087c5a3f76dfa15833aa1628d56afdf13
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-11-28 11:41:28 +0900

    オーバーロード 例題

---
+++ JavaStudy/overLoad/src/overLoad/SumMain.java
@@ -0,0 +1,12 @@
+package overLoad;
+
+public class SumMain {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Sum sum = new Sum();
+		int sumValue = sum.plus(1,2,3);
+		System.out.println(sumValue);
+	}
+
+}
