6fbb76b493f24906f2ba0de6caa3b824256d8799
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-11-30 10:26:03 +0900

    課題提出 2-6
    日付操作クラスについて

---
+++ JavaStudy/2-6/src/study/Study.java
@@ -0,0 +1,61 @@
+package study;
+import java.time.Year;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Study {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		// Dateクラスのインスタンスを生成
+        Date now = new Date();
+
+        // 問① ローカル変数「now」を使用し、本日の日付を表示してください。
+        System.out.println("---問1---");
+        System.out.println(now);
+
+        // Calendarクラスのインスタンスを生成
+        Calendar calendar = Calendar.getInstance();
+
+        // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
+        // その理由をコメントへ記述してください。
+        // [月の数字に+1しているだけなので、月の繰り上げや繰り下げの際に13月などといった月が表示されてしまうため]
+        System.out.println("---問2---");
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH) + 1);
+        System.out.println(calendar.get(Calendar.DATE));
+        // 問③ 上記の「calendar」を使用し、本日から1年2ヶ月15日先の日付を表示しなさい。
+        // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
+        // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
+        System.out.println("---問3---");
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.DATE));
+        
+        calendar.add(Calendar.YEAR, 1);
+        calendar.add(Calendar.MONTH, 2);
+        calendar.add(Calendar.DATE, 15);
+        
+        System.out.println(calendar.get(Calendar.YEAR));
+        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.DATE));
+        
+        
+
+
+
+
+        // うるう年判定
+        printLeapYear(calendar.get(Calendar.YEAR));
+    }
+
+    /**
+     * うるう年判定
+     * 問③で日付がズレる可能性があるため補足として出力
+     * @param year
+     */
+    private static void printLeapYear(final int year) {
+        System.out.println(year + "年は、うるう年" + (Year.isLeap(year) ? "です。" : "ではありません。"));
+	}
+
+}
