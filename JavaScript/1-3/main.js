45b522c149cfd76ab1427739caeabf83565411cb
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2021-04-10 06:08:03 +0900

    課題1-3

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,22 @@
+let a = 9;
+let b = 16;
+
+//問1
+a !== 10;
+b >= 10 && b <= 20 && b%2 === 0;
+
+//問２
+let x = 10;
+if(x >= 10 && x <= 20){
+  console.log("成功です");
+} else {
+  console.log('失敗です');
+}
+
+//問３
+let y = 0;
+if(y % 2 === 0){
+  console.log('偶数です');
+} else if(y % 2 !== 0){
+  console.log('奇数です');
+}
